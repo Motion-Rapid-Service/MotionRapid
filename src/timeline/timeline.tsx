cb85b047c4a35a9ab13bbdf500ab519bb672fda4
Shio3001 <taku2000maru@icloud.com>
2022-05-30 11:33:40 +0900

    node_modules掃除

--- src/timeline/timeline.tsx
+++ src/timeline/timeline.tsx
@@ -1,2 +1,11 @@
 import * as React from 'react';
 const { useContext, useReducer, createContext } = React;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const TimelineComponent = () => {
+    // ここでhooksを使える
+    return (<div className='timeline-area'>
+        <p>快速急行大阪上本町行きです</p>
+    </div>)
+  }
+export default TimelineComponent;
