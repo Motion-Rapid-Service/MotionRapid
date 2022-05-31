7865a6ffb1ce5fa4bb5be3eb3df48c28a0c25b2c
Shio3001 <taku2000maru@icloud.com>
2022-05-31 11:37:37 +0900

    mouseとstate

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
 const { useContext, useReducer, createContext } = React;
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from './App';
 
 
-ReactDOM.render(
-    
-    <App />,
-    
-    document.getElementById('root')
-);
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById('root')
+// );
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
--- src/timeline/timeline.tsx
+++ src/timeline/timeline.tsx
@@ -1,8 +1,11 @@
 import * as React from "react";
-const { useState, useContext, useReducer, createContext } = React;
+const { useState, useRef, useEffect, useContext, useReducer, createContext } =
+  React;
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./timeline.css";
 import "./keyframe.css";
+
+const UserHandTolerance = 3;
 
 const KeyFrameComponent = () => {
   return (
@@ -27,18 +30,71 @@ const MediaObjectScrollComponent = () => {
 };
 
 const MediaObjectAreaComponent = () => {
+  const mediaObjectAreaElement = useRef(null);
+  const [mouseX, mouseXSetState] = useState(0);
+  const [mouseY, mouseYSetState] = useState(0);
+
+  const timeLineMouseMoveAction = (event: any) => {
+    const mouseXY = timeLineMousePosition(event, mediaObjectAreaElement);
+    mouseXSetState(mouseXY[0])
+    mouseYSetState(mouseXY[1])
+  };
+
   return (
-    <div className="media_object-area">
+    <div className="media_object-area" ref={mediaObjectAreaElement} onMouseMove={timeLineMouseMoveAction}>
       <MediaObjectScrollComponent />
     </div>
   );
 };
 
+const timeLineMousePosition = (
+  event: any,
+//   timelineAreaElement: any,
+  mediaObjectAreaElement: any
+  //   timelineScrollElement: any
+) => {
+  const clientX = event.clientX;
+  const clientY = event.clientY;
+
+  const mediaObjectAreaElementBoundingClientRect =
+    mediaObjectAreaElement.current.getBoundingClientRect();
+
+  const mediaObjectElementLeft = mediaObjectAreaElementBoundingClientRect.left;
+  const mediaObjectElementTop = mediaObjectAreaElementBoundingClientRect.top;
+
+  //   const timelineAreaElementBoundingClientRect =
+  //     timelineAreaElement.current.getBoundingClientRect();
+
+  //   const areaElementLeft = timelineAreaElementBoundingClientRect.left;
+  //   const areaElementTop = timelineAreaElementBoundingClientRect.top;
+
+  //   const timelineScrollElementBoundingClientRect =
+  //     timelineScrollElement.current.getBoundingClientRect();
+
+  //   const scrollElementLeft = timelineScrollElementBoundingClientRect.left;
+  //   const scrollElementTop = timelineScrollElementBoundingClientRect.top;
+
+  const mouseAreaX = clientX - mediaObjectElementLeft;
+  const mouseAreaY = clientY - mediaObjectElementTop;
+  console.log(mouseAreaX, mouseAreaY);
+
+  return [mouseAreaX, mouseAreaY];
+};
+
 const TimelineComponent = () => {
   // ここでhooksを使える
+  const timelineAreaElement = useRef(null);
+  const timelineScrollElement = useRef(null);
+  useEffect(() => {}, []);
+
   return (
-    <div className="timeline-area">
-      <div className="timeline-area-scroll">
+    <div className="timeline-area" ref={timelineAreaElement}>
+      <div
+        className="timeline-area-scroll"
+        ref={timelineScrollElement}
+
+        // onScroll={TimeLineAreaMove}
+      >
         <MediaObjectAreaComponent />
         <MediaObjectAreaComponent />
         <MediaObjectAreaComponent />
