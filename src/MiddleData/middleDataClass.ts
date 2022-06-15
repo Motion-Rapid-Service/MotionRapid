1e81aab6af83120eae8819b5a28d74f419ab9332
Shio3001 <taku2000maru@icloud.com>
2022-06-15 11:30:02 +0900

    create整備

--- src/MiddleData/middleDataClass.ts
+++ src/MiddleData/middleDataClass.ts
@@ -1,38 +1,44 @@
-class DataCentral {
-  OwnedClass_Composite:Array<Composite>;
-  OwnedClass_MediaObject:Array<MediaObject>;
-  OwnedClass_Keyframe:Array<Keyframe>;
-  OwnedClass_Property:Array<Property>;
-  projectName:string;
-  constructor() {
+export class DataCentral {
+  // OwnedClass_Composite:Array<Composite>;
+  // OwnedClass_MediaObject:Array<MediaObject>;
+  // OwnedClass_Property:Array<Property>;
+  // OwnedClass_Keyframe:Array<Keyframe>;
+
+  OwnedClass_Composite: { [name: string]: Composite };
+  OwnedClass_MediaObject: { [name: string]: MediaObject };
+  OwnedClass_Property: { [name: string]: Property };
+  OwnedClass_Keyframe: { [name: string]: Keyframe };
+  projectName: string;
+
+  constructor(send_projectName:string) {
     // this.Composite = null;
     // this.MediaObject = null;
-    this.projectName = "";
-    this.OwnedClass_Composite = [];
-    this.OwnedClass_MediaObject = [];
-    this.OwnedClass_Property = [];
-    this.OwnedClass_Keyframe = [];
+    this.projectName = send_projectName;
+    this.OwnedClass_Composite = {};
+    this.OwnedClass_MediaObject = {};
+    this.OwnedClass_Property = {};
+    this.OwnedClass_Keyframe = {};
   }
 }
 
-class Composite {
-  Composite_ID:string;
-  Composite_Name:string;
-  OwnedID_MediaObject:Array<string>
-  constructor(send_Composite_ID:string,send_Composite_Name:string) {
+export class Composite {
+  Composite_ID: string;
+  Composite_Name: string;
+  OwnedID_MediaObject: Array<string>;
+  constructor(send_Composite_ID: string, send_Composite_Name: string) {
     this.Composite_ID = send_Composite_ID;
     this.Composite_Name = send_Composite_Name;
     this.OwnedID_MediaObject = [];
   }
 }
 
-class MediaObject {
-  MediaObject_ID:string
-  MediaObject_StartTime:number
-  MediaObject_EndTime:number
-  MediaObject_LayerNumber:number
-  OwnedID_Property:Array<string>
-  constructor(send_MediaObject_ID:string) {
+export class MediaObject {
+  MediaObject_ID: string;
+  MediaObject_StartTime: number;
+  MediaObject_EndTime: number;
+  MediaObject_LayerNumber: number;
+  OwnedID_Property: Array<string>;
+  constructor(send_MediaObject_ID: string) {
     this.MediaObject_ID = send_MediaObject_ID;
     this.MediaObject_StartTime = 0;
     this.MediaObject_EndTime = 0;
@@ -41,20 +47,19 @@ class MediaObject {
   }
 }
 
-class Property {
-  Property_ID:string;
-  OwnedID_Keyframe:string;
-  constructor(send_Property_ID:string) {
+export class Property {
+  Property_ID: string;
+  OwnedID_Keyframe: Array<string>;
+  constructor(send_Property_ID: string) {
     this.Property_ID = send_Property_ID;
-    this.OwnedID_Keyframe = "";
+    this.OwnedID_Keyframe = [];
   }
 }
 
-
-class Keyframe {
-  Keyframe_ID:string;
-  Keyframe_AbsoluteTime:number;
-  constructor(send_Keyframe_ID:string) {
+export class Keyframe {
+  Keyframe_ID: string;
+  Keyframe_AbsoluteTime: number;
+  constructor(send_Keyframe_ID: string) {
     this.Keyframe_ID = send_Keyframe_ID;
     this.Keyframe_AbsoluteTime = 0;
   }
--- src/MiddleData/middleDataOperation.ts
+++ src/MiddleData/middleDataOperation.ts
@@ -1,9 +1,10 @@
-const middleDataClass = require("./middleDataClass") as any;
+// const middleDataClass = require("./middleDataClass") as any;
+import * as middleDataClass from "./middleDataClass"
 import UUID from "uuidjs";
 
 const getUUID = () => {
-  return String(UUID.generate())
-}
+  return String(UUID.generate());
+};
 
 export class MiddleDataOperation {
   DataCentral: any;
@@ -11,45 +12,68 @@ export class MiddleDataOperation {
     this.DataCentral = null;
   }
 
-  existenceInquiryDataCentral = () =>{ //DataCentralが生成されているか参照する
-    return this.DataCentral !== null
-  }
+  existenceInquiryDataCentral = () => {
+    //DataCentralが生成されているか参照する
+    return this.DataCentral !== null;
+  };
 
   createDataCentral = (projectName: string = getUUID()) => {
-    this.DataCentral = new middleDataClass(projectName);
+    this.DataCentral = new middleDataClass.DataCentral(projectName);
   };
 
   createComposite = () => {
-    const newID  = getUUID();
-    const newObj = new middleDataClass.Composite(newID,"test_composite");
-    this.DataCentral.OwnedClass_Composite.push(newObj);
+    const newID = getUUID();
+    const newObj = new middleDataClass.Composite(newID, "test_composite");
+    this.DataCentral.OwnedClass_Composite[newID] = newObj;
   };
 
   createMediaObject = () => {
-    const newID  = getUUID();
+    const newID = getUUID();
     const newObj = new middleDataClass.MediaObject(newID);
-    this.DataCentral.OwnedClass_MediaObject.push(newObj);
+    this.DataCentral.OwnedClass_MediaObject[newID] = newObj;
   };
 
   createKeyframe = () => {
-    const newID  = getUUID();
+    const newID = getUUID();
     const newObj = new middleDataClass.Keyframe(newID);
-    this.DataCentral.OwnedClass_Keyframe.push(newObj);
+    this.DataCentral.OwnedClass_Keyframe[newID] = newObj;
   };
 
   createProperty = () => {
-    const newID  = getUUID();
+    const newID = getUUID();
     const newObj = new middleDataClass.Property(newID);
-    this.DataCentral.OwnedClass_Property.push(newObj);
+    this.DataCentral.OwnedClass_Property[newID] = newObj;
   };
 
-  linkMediaObject  = () => {
+  linkMediaObject = (compositeID:string,mediaObjectID:string) => {
+    this.DataCentral.OwnedClass_Composite[compositeID].OwnedID_MediaObject.push(mediaObjectID)
+  };
+  linkProperty = (mediaObjectID:string,propertyID:string) => {
+    this.DataCentral.OwnedClass_MediaObject[mediaObjectID].OwnedID_Property.push(propertyID)
+  };
+  linkKeyframe = (propertyID:string,keyframeID:string) => {
+    this.DataCentral.OwnedClass_Property[propertyID].OwnedID_Keyframe.push(keyframeID)
+  };
 
+  copyMediaObject = () => {};
+  copyKeyframe = () => {};
+  copyProperty = () => {};
+
+  deleteMediaObject = () => {};
+  deleteKeyframe = () => {};
+  deleteProperty = () => {};
+
+  layerMaximum = (compositeID:string) => {
+    const targetMediaObjectLengh = this.DataCentral.OwnedClass_Composite[compositeID].OwnedID_MediaObject.lengh
+
+    // const nowMax = 0;
+
+    // for (let i = 0; i < targetMediaObjectLengh ;i++ )
+    // {
+    //   MediaObjectValue[i]
+    // }
   }
-  linkKeyframe  = () => {
-    
-  }
-  linkProperty  = () => {
+  layerNormalization = (compositeID:string) => { //たぶん計算量がn^2ぐらいになりそう
     
   }
 }
